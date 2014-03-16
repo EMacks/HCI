16de30f8e9dbce43da875400c8805556c91abecf
Camara Lerner <camara.lerner@gmail.com>
2014-03-16 14:38:28 -0600

    fixed an uninitiallize pointer

--- src/HCI/emotionrank.cpp
+++ src/HCI/emotionrank.cpp
@@ -7,17 +7,17 @@
 #include "emotionrank.h"
 
 EmotionRank::EmotionRank(QString em,QWidget *p){
-    QGridLayout *layout;
-    QLabel *label=new QLabel(em);
-    layout->addWidget(label);
-
-    radios.append(new QRadioButton("Strongly Agree",this));
-    radios.append(new QRadioButton("Agree",this));
-    radios.append(new QRadioButton("Neutral",this));
-    radios.append(new QRadioButton("Disagree",this));
-    radios.append(new QRadioButton("Strongly Disagree",this));
-    for(int i=0;i<radios.length();i++){
-        layout->addWidget(radios.at(i),0,i+1,1,1);
-    }
-    setLayout(layout);
+   QGridLayout *layout = new QGridLayout();
+   QLabel *label=new QLabel(em);
+   layout->addWidget(label);
+   
+   radios.append(new QRadioButton("Strongly Agree",this));
+   radios.append(new QRadioButton("Agree",this));
+   radios.append(new QRadioButton("Neutral",this));
+   radios.append(new QRadioButton("Disagree",this));
+   radios.append(new QRadioButton("Strongly Disagree",this));
+   for(int i=0;i<radios.length();i++){
+      layout->addWidget(radios.at(i),0,i+1,1,1);
+   }
+   setLayout(layout);
 }
