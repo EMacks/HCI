45485784294e3f7b553d24ae8a0cbc0026c0a97a
Camara Lerner <camara.lerner@gmail.com>
2014-04-06 14:46:45 -0600

    database change

--- src/HCI/database.h
+++ src/HCI/database.h
@@ -13,22 +13,24 @@ bool makeConnection()
 {
    QStringList drivers = QSqlDatabase::drivers();
    qDebug() << drivers;
-/*    QSqlDatabase db=QSqlDatabase::addDatabase("QPSQL");
+   QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
     //db.setHostName("hci.ccmrejgq3bfa.us-west-2.rds.amazonaws.com");
     db.setHostName("");
     db.setPort(5432);
     db.setDatabaseName("HCI");
     db.setUserName("sqluser");
-    db.setPassword("password123");
-*/
-   QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-   db.setDatabaseName("Driver={SQL Server Native Client 10.0};Server=tcp:l2ud2zp8th.database.windows.net,1433;Database=HCI_PROJECT;Encrypt=yes;Connection Timeout=30;");
+    db.setPassword("12curLING*$");
+    
+   /*QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+     db.setDatabaseName("Driver={SQL Server Native Client 10.0};Server=tcp:l2ud2zp8th.database.windows.net,1433;Database=HCI_PROJECT;Encrypt=yes;Connection Timeout=30;");
+   */
 /* db.setHostName("");
    db.setPort(1433);
    db.setDatabaseName("HCI");
 */ 
-   db.setUserName("sqluser");
+/*   db.setUserName("sqluser");
    db.setPassword("12curLING*$");
+*/
    return db.open();
 //   db.setDatabaseName("Driver={SQL Server Native Client 10.0};Server=tcp:l2ud2zp8th.database.windows.net,1433;Database=HCI_PROJECT;Encrypt=yes;Connection Timeout=30;");
 /*    db.setUserName("sqluser@l2ud2zp8th");
