bf238e0f14bd0f60bacef418ea3ac06077b3e60f
Camara Lerner <camara.lerner@gmail.com>
2014-04-06 14:54:36 -0600

    ignoring database for now

--- src/HCI/main.cpp
+++ src/HCI/main.cpp
@@ -11,10 +11,10 @@
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
-   if(DOTHIS()) {
+   // if(DOTHIS()) {
       MainWindow w;
-      w.show();
-   }
+      w.exec();
+      // }
    
    return a.exec();
 }
